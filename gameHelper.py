be32df6060d1d71c804c5279bde7accaa575edc3
S K Aravind <aravindraghvi@gmail.com>
2021-08-06 15:38:25 -0400

    Add game files

---
+++ gameHelper.py
@@ -0,0 +1,313 @@
+import sys
+
+class Node:
+	def __init__(self, board, depth):
+		self.position = board
+		self.depth = depth
+		self.code = ''.join(board)
+
+
+class MiniMaxGame:
+	def __init__(self, maxDepth):
+		self.evaluatedPositions = 0
+		self.bestResponse = None
+		self.maxDepth = maxDepth
+
+		self.neighbors = {
+			0: [1, 2, 6],
+			1: [0, 11],
+			2: [0, 4, 7, 3],
+			3: [2, 10],
+			4: [2, 5, 8],
+			5: [4, 9],
+			6: [0, 7, 18],
+			7: [2, 6, 8, 15],
+			8: [4, 7, 12],
+			9: [5, 10, 14],
+			10: [3, 9, 11, 17],
+			11: [1, 10, 20],
+			12: [8, 13],
+			13: [12, 14, 16],
+			14: [9, 13],
+			15: [7, 16],
+			16: [13, 15, 17, 19],
+			17: [10, 16],
+			18: [6, 19],
+			19: [16, 18, 20],
+			20: [11, 19]
+		}
+
+		self.checkMillMap = {
+			0: [[6,18],[2,4]],
+			1: [[11,20]],
+			2: [[7,15],[0,4]],
+			3: [[10,17]],
+			4: [[0,2],[8,12]],
+			5: [[9,14]],
+			6: [[0,18],[7,8]],
+			7: [[6,8],[2,15]],
+			8: [[6,7],[4,12]],
+			9: [[5,14],[10,11]],
+			10: [[9,11],[3,17]],
+			11: [[9,10],[1,20]],
+			12: [[8,4],[13,14]],
+			13: [[12,14],[16,19]],
+			14: [[5,9],[12,13]],
+			15: [[2,7],[16,17]],
+			16: [[15,17],[13,19]],
+			17: [[15,16],[10,3]],
+			18: [[0,6],[19,20]],
+			19: [[13,16],[18,20]],
+			20: [[18,19],[11,1]]
+		}
+
+
+		
+	def closeMill(self, j, b):
+		for millNeighbors in self.checkMillMap[j]:
+			if (b[millNeighbors[0]] == b[j]) and (b[millNeighbors[1]] == b[j]):
+				return True
+
+		return False
+
+
+	def potentialCloseMill(self, j, b):
+		for millNeighbors in self.checkMillMap[j]:
+			if (b[millNeighbors[0]] == b[j]) and (b[millNeighbors[1]] == 'x'):
+				return True
+			if (b[millNeighbors[0]] == 'x') and (b[millNeighbors[1]] == b[j]):
+				return True
+
+		return False
+
+
+	def countMills(self, board):
+		numBlackCloseMills = 0
+		numWhiteCloseMills = 0
+		numPotBlackCloseMills = 0
+		numPotWhiteCloseMills = 0
+
+		for loc in range(len(board)):
+			if board[loc] == 'W':
+				if self.closeMill(loc, board):
+					numWhiteCloseMills += 1
+				if self.potentialCloseMill(loc, board):
+					numPotWhiteCloseMills += 1
+			if board[loc] == 'B':
+				if self.closeMill(loc, board):
+					numBlackCloseMills += 1
+				if self.potentialCloseMill(loc, board):
+					numPotBlackCloseMills += 1
+		
+		return numWhiteCloseMills, numBlackCloseMills, \
+			numPotWhiteCloseMills, numPotBlackCloseMills
+
+
+	def countPieces(self, board):
+		numWhitePieces = 0
+		numBlackPieces = 0
+
+		for b in board:
+			if b=='W':
+				numWhitePieces += 1
+			if b=='B':
+				numBlackPieces += 1
+
+		return numWhitePieces, numBlackPieces
+
+	def checkBlocked(self, board, loc):
+		res = True
+		n = self.neighbors[loc]
+		for ni in n:
+			if board[ni] == 'x':
+				res = False
+		return res
+
+	def blocked(self, board):
+		whiteBlocked = 0
+		blackBlocked = 0
+
+		for loc in range(len(board)):
+			if board[loc] == 'W':
+				if self.checkBlocked(board, loc):
+					whiteBlocked += 1
+			if board[loc] == 'B':
+				if self.checkBlocked(board, loc):
+					blackBlocked += 1
+		
+		return whiteBlocked - blackBlocked	
+
+	def static(self, board):
+		numWhitePieces = 0
+		numBlackPieces = 0
+		for b in board:
+			if b=='W':
+				numWhitePieces += 1
+			if b=='B':
+				numBlackPieces += 1
+
+		numBlackMoves = len(self.GenerateMovesMidgameEndgame(board, True))
+
+		numWhiteCloseMills, numBlackCloseMills, \
+		numPotWhiteCloseMills, numPotBlackCloseMills = self.countMills(board)
+
+		if numBlackPieces <= 2:
+			return 10000
+		elif numWhitePieces <= 2:
+			return -10000
+		elif numBlackMoves == 0:
+			return 10000
+		else:
+			return 100 * ((numWhitePieces - numBlackPieces) + \
+				3*(numWhiteCloseMills - numBlackCloseMills) + \
+				2*(numPotWhiteCloseMills - numPotBlackCloseMills) + \
+				self.blocked(board)) - numBlackMoves
+
+
+	def MinMax(self, x, alpha, beta):
+		depth = x.depth
+		if self.maxDepth == depth:
+			self.evaluatedPositions += 1
+			return self.static(x.position)
+
+		
+		v = 50000 - depth
+		children = self.GenerateMovesMidgameEndgame(x.position, switchColor=True)
+
+		for y in children:
+			node_y = Node(y, depth+1)
+			v = min(v, self.MaxMin(node_y, alpha, beta))
+			
+			if v <= alpha:
+				return v
+			else:
+				beta = min(v,beta)
+		return v
+
+
+	def MaxMin(self, x, alpha, beta):
+		depth = x.depth
+		if self.maxDepth == depth:
+			self.evaluatedPositions += 1
+			return self.static(x.position)
+		
+		v = -50000 + depth
+		children = self.GenerateMovesMidgameEndgame(x.position)
+
+		for y in children:
+			node_y = Node(y, depth+1)
+			tmpV = v
+			v = max(v, self.MinMax(node_y, alpha, beta))
+			
+			if v > tmpV and node_y.depth==1:
+					self.bestResponse = y
+			
+			if v >= beta:
+				return v
+			else:
+				alpha = max(v,alpha)
+
+		return v
+
+
+	def switchColors(self, board):
+		for i in range(len(board)):
+			if type(board[i]) == list:
+				for j in range(len(board[i])):
+					if board[i][j] == 'W':
+						board[i][j] = 'B'
+					elif board[i][j] == 'B':
+						board[i][j] = 'W'
+
+			else:
+				if board[i] == 'W':
+					board[i] = 'B'
+				elif board[i] == 'B':
+					board[i] = 'W'
+		return board
+
+
+
+	def GenerateRemove(self, board, L):
+		numPositions = 0
+
+		for loc in range(len(board)):
+			if board[loc] == 'B':
+				if not self.closeMill(loc, board):
+					b = board.copy()
+					b[loc] = 'x'
+					L.append(b)
+					numPositions += 1
+
+		if numPositions == 0:
+			L.append(board.copy())
+
+
+	def GenerateAdd(self, board):
+		L = []
+
+		for loc in range(len(board)):
+			if board[loc] == 'x':
+				b = board.copy()
+				b[loc] = 'W'
+				if self.closeMill(loc, b):
+					self.GenerateRemove(b, L)
+				else:
+					L.append(b)
+		
+		return L
+
+
+
+	def GenerateHopping(self, board):
+		L = []
+		for loc1 in range(len(board)):
+			if board[loc1] == 'W':
+				for loc2 in range(len(board)):
+					if board[loc2] == 'x':
+						b = board.copy()
+						b[loc1] = 'x'
+						b[loc2] = 'W'
+						if self.closeMill(loc2, b):
+							self.GenerateRemove(b, L)
+						else:
+							L.append(b)
+		return L
+
+
+	def GenerateMove(self, board):
+		L = []
+		for loc in range(len(board)):
+			if board[loc] == 'W':
+				n = self.neighbors[loc]
+				for j in n:
+					if board[j] == 'x':
+						b = board.copy()
+						b[loc] = 'x'
+						b[j] = 'W'
+						if self.closeMill(j,b):
+							self.GenerateRemove(b, L)
+						else:
+							L.append(b)
+		return L
+
+
+	def GenerateMovesMidgameEndgame(self, board, switchColor=False):
+		if switchColor:
+			board = self.switchColors(board)
+
+		numWhitePieces = 0
+		for b in board:
+			if b=='W':
+				numWhitePieces += 1
+
+		if numWhitePieces == 3:
+			L = self.GenerateHopping(board)
+		else:
+			L = self.GenerateMove(board)
+
+		if switchColor:
+			board = self.switchColors(board)
+			L = self.switchColors(L)
+		
+		return L
---
+++ morris.py
@@ -0,0 +1,326 @@
+import pygame
+from openingHelper import *
+from gameHelper import *
+from time import sleep
+import subprocess as sb
+
+pygame.init()
+
+screen = pygame.display.set_mode((500, 630))
+
+pygame.display.set_caption("Nine Men Morris")
+
+#images
+boardImg = pygame.image.load('static/morrisSmall.png')
+leafImg = pygame.image.load('static/green1.png')
+fireImg = pygame.image.load('static/fire1.png')
+highImg = pygame.image.load('static/high.png')
+roboImg = pygame.image.load('static/robo1.png')
+
+coords = {
+	0: (70, 720, 120, 770),
+	1: (770, 720, 820, 770),
+	2: (180, 610, 230, 660),
+	3: (660, 610, 710, 660),
+	4: (300, 490, 350, 540),
+	5: (540, 490, 590, 540),
+	6: (70, 375, 120, 425),
+	7: (180, 375, 230, 425),
+	8: (300, 375, 350, 425),
+	9: (540, 375, 590, 425),
+	10: (660, 375, 710, 425),
+	11: (770, 375, 820, 425),
+	12: (300, 260, 350, 310),
+	13: (420, 260, 470, 310),
+	14: (540, 260, 590, 310),
+	15: (180, 140, 230, 190),
+	16: (420, 140, 470, 190),
+	17: (660, 140, 710, 190),  
+	18: (70, 30, 120, 80),
+	19: (420, 30, 470, 80),
+	20: (770, 30, 820, 80)
+}
+
+mul = 500/843
+
+clickables = [pygame.Rect(mul*c[0], mul*c[1], 35, 35) for c in coords.values()]
+
+board = list('xxxxxxxxxxxxxxxxxxxxx')
+
+turn = 2
+v = 0
+
+running = True
+mill = False
+played = False
+moveLoc = None
+
+MAX = 50000
+
+Game = MiniMaxOpening(9, turn)
+
+Font = pygame.font.SysFont('Roboto Mono',  30)
+roboFont = pygame.font.SysFont('Roboto Mono',  25)
+
+openingText = Font.render("OPENING PHASE", False, (0,0,0))
+middleGameText = Font.render("MIDDLE GAME", False, (0,0,0))
+endgameText = Font.render("ENDGAME", False, (0,0,0))
+overText = Font.render("END!", False, (0,0,0))
+sb.Popen(["python", "static/robo2.py"])
+waitText = roboFont.render(" : Ok, let me think . . . . . . . .", False, (0,0,0))
+millText = roboFont.render(" : Ah! Mill! Go ahead and remove my piece", False, (0,0,0))
+openingRobo = roboFont.render(" : Place a piece on any empty vertice", False, (0,0,0))
+middleRobo = roboFont.render(" : Click on your piece and move to an adjacent spot", False, (0,0,0))
+endGameRobo = roboFont.render(" : Click on your piece and move to any spot", False, (0,0,0))
+overRobo1 = roboFont.render(" : I accept my defeat. GG. Press R to restart", False, (0,0,0))
+overRobo2 = roboFont.render(" : As expected. GG. Press R to restart", False, (0,0,0))
+winningRobo = roboFont.render(" : I am winning this easily.", False, (255,0,0))
+losingRobo = roboFont.render(" : Ah, you might actually win this.", False, (0,255,0))
+drawingRobo = roboFont.render(" : This could be a draw", False, (0,0,255))
+
+text_rect = openingText.get_rect(center=(250, 510))
+
+selectMove = False
+availableShifts = []
+endGame = False
+gameComplete = 0
+
+
+fps = 20
+clock = pygame.time.Clock()
+
+def checkEndgame():
+	global endGame
+	endGame = False
+	cnt = 0
+	for b in board:
+		if b == 'B':
+			cnt += 1
+	if cnt == 3:
+		endGame = True
+
+def checkGameComplete():
+	global gameComplete
+
+	if turn > 18:
+		cnt1, cnt2 = 0, 0
+		for b in board:
+			if b == 'B':
+				cnt1 += 1
+			if b == 'W':
+				cnt2 += 1
+		if cnt2 < 3:
+			gameComplete = -1 
+		if cnt1 < 3:
+			gameComplete = 1
+
+		Game = MiniMaxGame(3)
+		
+		if Game.GenerateMovesMidgameEndgame(board, True) == []:
+			gameComplete = -1
+		if Game.GenerateMovesMidgameEndgame(board) == []:
+			gameComplete = 1
+
+
+def drawBoard():
+	global availableShifts, mill, moveLoc
+	if selectMove:
+		if endGame:
+			availableShifts = []
+			for loc in range(len(board)):
+				if board[loc] == 'x':
+					availableShifts.append(loc)
+					x = mul*coords[loc][0] - 5
+					y = mul*coords[loc][1] - 5
+					screen.blit(highImg,(x, y))
+
+		else:
+			n = Game.neighbors[moveLoc]
+			availableShifts = []
+			for j in n:
+				if board[j] == 'x':
+					availableShifts.append(j)
+					x = mul*coords[j][0] - 5
+					y = mul*coords[j][1] - 5
+					screen.blit(highImg,(x, y))		
+
+
+	if mill:
+		cnt = 0
+		for loc in range(len(board)):
+			if board[loc] == 'W':
+				if not Game.closeMill(loc, board):
+					x = mul*coords[loc][0] - 5
+					y = mul*coords[loc][1] - 5
+					screen.blit(highImg,(x, y))
+					cnt += 1
+		if cnt == 0:
+			moveLoc = None
+			mill = False
+
+	
+	for loc in range(len(board)):
+		if board[loc] == 'W':
+			x = mul*coords[loc][0]
+			y = mul*coords[loc][1]
+			screen.blit(fireImg,(x, y))
+		if board[loc] == 'B':
+			x = mul*coords[loc][0]
+			y = mul*coords[loc][1]
+			screen.blit(leafImg,(x, y))
+
+
+def drawText():
+	screen.blit(roboImg, (10, 540))
+	
+	if gameComplete == 1:
+		screen.blit(overText, text_rect)
+		screen.blit(overRobo1, (50, 550))
+	if gameComplete == -1:
+		screen.blit(overText, text_rect)
+		screen.blit(overRobo2, (50, 550))
+
+	else:
+		if turn <= 18:
+			screen.blit(openingText, text_rect)
+		elif endGame:
+			screen.blit(endgameText, text_rect)
+		else:
+			screen.blit(middleGameText, text_rect)
+
+		if played and (not mill):
+			screen.blit(waitText, (50, 550))
+
+		elif mill:
+			screen.blit(millText, (50, 550))
+
+		else:
+			if turn <= 18:
+				screen.blit(openingRobo, (50, 550))
+			elif endGame:
+				screen.blit(endGameRobo, (50, 550))
+			elif not gameComplete:
+				screen.blit(middleRobo, (50, 550))
+
+			if turn >= 14:
+				if v > 150:
+					screen.blit(winningRobo, (50, 590))
+				elif v < -150:
+					screen.blit(losingRobo, (50, 590))
+				else:
+					screen.blit(drawingRobo, (50, 590))
+
+
+while running:
+	screen.fill((255, 255, 255))
+	screen.blit(boardImg, (0, 0))
+	checkEndgame()
+	checkGameComplete()
+	
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
+		# opening
+		if (not mill) and turn <= 18 and event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:  # Left mouse button.
+				# Check if the rect collides with the mouse pos.
+				for i, area in enumerate(clickables):
+					if area.collidepoint(event.pos):
+						board[i] = 'B'
+						played = True
+						moveLoc = i
+		
+		# midgame
+		if selectMove and turn > 18 and event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:  # Left mouse button.
+				# Check if the rect collides with the mouse pos.
+				for i, area in enumerate(clickables):
+					if area.collidepoint(event.pos):
+						if i in availableShifts:
+							board[i] = 'B'
+							board[moveLoc] = 'x'
+							played = True
+							turn += 2
+							moveLoc = i
+							selectMove = False
+
+
+		if (not played) and (not mill) and turn > 18 and event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:  # Left mouse button.
+				# Check if the rect collides with the mouse pos.
+				for i, area in enumerate(clickables):
+					if area.collidepoint(event.pos):
+						if board[i] == 'B':
+							turn += 2
+							moveLoc = i
+							selectMove = True
+		
+		
+		# mill logic
+		if mill and event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 1:  # Left mouse button.
+				# Check if the rect collides with the mouse pos.
+				for i, area in enumerate(clickables):
+					if area.collidepoint(event.pos):
+						if board[i] == 'W':
+							board[i] = 'x'
+							mill = False
+							moveLoc = None
+
+		# restart logic
+		if gameComplete != 0 and event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_r:
+				gameComplete = False
+				played = False
+				board = list('xxxxxxxxxxxxxxxxxxxxx')
+				moveLoc = None
+				turn = 2
+
+	drawBoard()
+	drawText()
+	
+	pygame.display.update()
+	clock.tick(fps)
+
+
+	if played and (not mill) and (not selectMove):
+		if moveLoc and Game.closeMill(moveLoc, board):
+			#print('Mill! remove opponent piece.')
+			mill = True
+			continue
+
+	if turn <= 18 and (not mill) and played:
+		Game = MiniMaxOpening(7, turn)
+
+
+		root = Node(board, turn)
+		v = Game.MaxMin(root, -MAX, MAX)
+		#print(v, turn)
+		board = Game.bestResponse
+		played = False
+		turn += 2
+
+	if turn >= 18 and (not mill) and (not selectMove) and played:
+		if endGame:
+			Game = MiniMaxGame(4)
+		else:
+			Game = MiniMaxGame(5)
+		
+		root = Node(board, 0)
+		v = Game.MaxMin(root, -MAX, MAX)
+		
+		if v == float('inf'):
+			#print('inf detected')
+			Game = MiniMaxGame(3)
+			root = Node(board, 0)
+			v = Game.MaxMin(root, -MAX, MAX)
+		
+		#print(v, turn)
+		board = Game.bestResponse
+		played = False
+		turn += 2
+
+		
+
+
